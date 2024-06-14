d3c41827bc75580c8571c9702bc73b032eb181fd
Luis Fernandez <fernandezluisn@gmail.com>
2024-06-14 15:38:50 -0300

    Actualización

--- main.js
+++ main.js
@@ -121,7 +121,7 @@ async function generateCards() {
     const container = document.getElementById('muestrario');
 
     try {
-        const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=20');
+        const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=40');
         const data = await response.json();
 
         for (const pokemon of data.results) {
